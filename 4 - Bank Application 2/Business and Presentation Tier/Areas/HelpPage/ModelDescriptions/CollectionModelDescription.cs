b2d368af29b01e1d55375b37761e9728fd088226
Seth Lin <sethfire404@gmail.com>
2021-05-22 22:07:41 +0800

    Test2

--- 4 - Bank Application 2/Business and Presentation Tier/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Business_and_Presentation_Tier.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
